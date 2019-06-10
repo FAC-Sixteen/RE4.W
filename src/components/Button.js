bbadfd3b4227cf2be9018c395c15640a10a25771
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-10 17:48:43 +0100

    add onclick to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const StyledButton = styled.button`
     box-shadow: 4px 8px 4px rgba(0, 0, 0, 0.25);
@@ -13,10 +13,14 @@ const StyledButton = styled.button`
 border: 4px solid black;
 box-sizing: border-box;
 border-radius: 3em;
-`
+`;
 
 const Button = props => {
-    return <StyledButton data-testid="button">{props.children}</StyledButton>
-}
+    return (
+        <StyledButton onClick={props.onClick} data-testid="button">
+            {props.children}
+        </StyledButton>
+    );
+};
 
-export default Button
+export default Button;
