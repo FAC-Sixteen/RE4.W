1d10546eb10e017b4f9ab7617db602375d1ee516
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-18 11:06:11 +0100

    add AIRTABLE_BASE as env var

--- .netlify/functions/getData.js
+++ .netlify/functions/getData.js
@@ -1,42 +1,43 @@
 const { config } = require("dotenv");
 config();
-const Airtable = require('airtable');
+const Airtable = require("airtable");
 
 Airtable.configure({
-  endpointUrl: 'https://api.airtable.com',
-  apiKey: process.env.AIRTABLE_KEY
-})
+    endpointUrl: "https://api.airtable.com",
+    apiKey: process.env.AIRTABLE_KEY,
+});
 
-const base = Airtable.base('appkLPDVgr4TwR3lM');
+const base = Airtable.base(process.env.AIRTABLE_BASE);
 
 exports.handler = (event, context, callback) => {
-  const allRecords = []
-    base('Imported table')
-      .select({
-        view: "Grid view"
-      })
-      .eachPage(
-        page = (records, fetchNextPage) => {
-          records.forEach(record => {
-            allRecords.push(record)
-          })
-          fetchNextPage()
-        },
-        done = (err) => {
-          if (err) {
-            callback(err)
-          } else {
-            const body = JSON.stringify({ records: allRecords })
-            const response = {
-              statusCode: 200,
-              body: body,
-              headers: {
-                'content-type': 'application/json',
-                'cache-control': 'Cache-Control: max-age=300, public'
-              }
-            }
-            callback(null, response)
-          }
-        }
-      )
-  }
+    const allRecords = [];
+    base("Imported table")
+        .select({
+            view: "Grid view",
+        })
+        .eachPage(
+            (page = (records, fetchNextPage) => {
+                records.forEach(record => {
+                    allRecords.push(record);
+                });
+                fetchNextPage();
+            }),
+            (done = err => {
+                if (err) {
+                    callback(err);
+                } else {
+                    const body = JSON.stringify({ records: allRecords });
+                    const response = {
+                        statusCode: 200,
+                        body: body,
+                        headers: {
+                            "content-type": "application/json",
+                            "cache-control":
+                                "Cache-Control: max-age=300, public",
+                        },
+                    };
+                    callback(null, response);
+                }
+            })
+        );
+};
