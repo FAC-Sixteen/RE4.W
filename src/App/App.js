cae6cfd9ad59aaf696c1052366201b4bdad3ab79
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-18 17:39:38 +0100

    remove link from react-router-dom import

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Game from "../components/game/GameContainer";
 import Main from "../components/main/Main";
