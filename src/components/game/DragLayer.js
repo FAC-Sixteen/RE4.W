1f9443298a8e66e7ded59ccde283788bcd7958f0
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-19 23:16:08 +0100

    fixed offset of touch drag - broke bin drop :shrug

--- src/components/game/DragLayer.js
+++ src/components/game/DragLayer.js
@@ -11,17 +11,29 @@ const collect = monitor => {
 };
 
 const DragPreview = props => {
-    const getLayerStyles = () => {
-        const { sourceOffset } = props;
+    const [dragStart, setDragStart] = React.useState(0);
+    const { sourceOffset } = props;
 
+    React.useEffect(() => {
+        if (dragStart === 0 && sourceOffset !== null) {
+            setDragStart(sourceOffset);
+        } else if (sourceOffset === null) {
+            setDragStart(0);
+        }
+    }, [sourceOffset, dragStart]);
+
+    const getLayerStyles = () => {
+        console.log(sourceOffset, dragStart);
         return {
             transform: sourceOffset
-                ? `translate(${sourceOffset.x}px, ${sourceOffset.y}px)`
+                ? `translate(${sourceOffset.x -
+                      dragStart.x}px, ${sourceOffset.y - dragStart.y}px)`
                 : "",
         };
     };
 
     const transform = getLayerStyles();
+    console.log(transform);
 
     const { isDragging } = props;
     if (isDragging) {
