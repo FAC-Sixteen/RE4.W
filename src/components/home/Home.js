10dc8c16c07b04a352783f86458a76ced73c4da2
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-20 16:25:33 +0100

    remove unnecessary code from Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,31 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import fetchData from "../../utils/fetchData";
-
 import { StyledHome, StyledLogo, StyledLogoTitle } from "./Home.style";
-import { LargeButton, InactiveButton } from "../button/Button";
+import { LargeButton } from "../button/Button";
 import Logo from "../../assets/RecyclingHeroesLogo.png";
 import LogoTitle from "../../assets/RecyclingHeroesTitle.png";
 
 const Home = () => {
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetchData(setData);
-    }, []);
-
-    if (!data)
-        return (
-            <React.Fragment>
-                <StyledHome>
-                    <StyledLogo src={Logo} />
-                    <StyledLogoTitle src={LogoTitle} />
-                    <InactiveButton>Loading...</InactiveButton>
-                </StyledHome>
-            </React.Fragment>
-        );
-
     return (
         <React.Fragment>
             <StyledHome>
@@ -34,7 +15,6 @@ const Home = () => {
                 <Link
                     to={{
                         pathname: "/game",
-                        data: data,
                     }}
                 >
                     <LargeButton>Start Game</LargeButton>
