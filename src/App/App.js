4083a77bf6ff83531e1d3026747e5ffb8d0cc860
Samantha Dickeson <samlucydickeson@gmail.com>
2019-06-20 15:43:08 +0100

    move data fetch and loading screen to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,46 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import fetchData from "../utils/fetchData";
+
 import Game from "../components/game/GameContainer";
 import Home from "../components/home/Home";
 import Results from "../components/results/Results";
 import FactPage from "../components/factPage/FactPage";
 
+import {
+    StyledHome,
+    StyledLogo,
+    StyledLogoTitle,
+} from "../components/home/Home.style";
+import { InactiveButton } from "../components/button/Button";
+import Logo from "../assets/RecyclingHeroesLogo.png";
+import LogoTitle from "../assets/RecyclingHeroesTitle.png";
+
 function App() {
+    const [data, setData] = React.useState(null);
+
+    React.useEffect(() => {
+        fetchData(setData);
+    }, []);
+
+    if (!data)
+        return (
+            <React.Fragment>
+                <StyledHome>
+                    <StyledLogo src={Logo} />
+                    <StyledLogoTitle src={LogoTitle} />
+                    <InactiveButton>Loading...</InactiveButton>
+                </StyledHome>
+            </React.Fragment>
+        );
     return (
         <Router>
             <Route exact path="/" component={Home} />
-            <Route path="/game" component={Game} />
+            <Route
+                path="/game"
+                render={props => <Game {...props} data={data} />}
+            />
             <Route path="/factpage" component={FactPage} />
             <Route path="/results" component={Results} />
         </Router>
