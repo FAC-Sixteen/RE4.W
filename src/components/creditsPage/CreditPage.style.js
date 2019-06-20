84d511e46c055e4ddb82515c7a500dadbab3a35a
seabasshoang <sylviahoang@hotmail.co.uk>
2019-06-20 11:33:38 +0100

    add designer links Fixed #54

--- src/components/creditsPage/CreditPage.style.js
+++ src/components/creditsPage/CreditPage.style.js
@@ -7,13 +7,28 @@ const StyledCreditsPage = styled.div`
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center center;
-    height: 100vh;
+    min-height: 100vh;
+    width: 100vw;
+    position: absolute;
     top: 0;
     text-align: center;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    font-family: bangers;
+    font-size: 2em;
 `;
 
-export { StyledCreditsPage };
+const CreditText = styled.div`
+    width: 60vw;
+`;
+
+const CreditLink = styled.a`
+    border: 3px solid black;
+    background-color: white;
+    text-decoration: none;
+    color: black;
+`;
+
+export { StyledCreditsPage, CreditLink, CreditText };
--- src/components/creditsPage/CreditsPage.js
+++ src/components/creditsPage/CreditsPage.js
@@ -2,14 +2,27 @@ import React from "react";
 
 import Header from "../header/Header";
 import Main from "../main/Main";
-import { StyledCreditsPage } from "./CreditPage.style";
+import { StyledCreditsPage, CreditLink, CreditText } from "./CreditPage.style";
 
 const CreditsPage = () => {
     return (
         <React.Fragment>
             <Header text="Credits" />
             <Main>
-                <div>credits here</div>
+                <StyledCreditsPage>
+                    <CreditText>
+                        Icons and backgrounds made by{" "}
+                        <CreditLink href="https://www.freepik.com/">
+                            {" "}
+                            Freepik
+                        </CreditLink>{" "}
+                        from{" "}
+                        <CreditLink href="www.flaticons.com">
+                            {" "}
+                            www.flaticons.com
+                        </CreditLink>
+                    </CreditText>
+                </StyledCreditsPage>
             </Main>
         </React.Fragment>
     );
